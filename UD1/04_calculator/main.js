1343770dea453171ea6f0d1b8636d367e3e073ad
meeeww <thezasm03@gmail.com>
2023-09-25 18:11:16 +0200

    Prettier

--- UD1/04_calculator/main.js
+++ UD1/04_calculator/main.js
@@ -4,12 +4,12 @@
 // Multiplicación
 // División
 
-
 // define variables
-let x = 5, y = 2;
+let x = 5,
+  y = 2;
 
 // print sum, substract, multiply and divide
-console.log(`Suma: ${x + y}`)
-console.log(`Resta: ${x - y}`)
-console.log(`Multiplicación: ${x * y}`)
-console.log(`División: ${x / y}`)
+console.log(`Suma: ${x + y}`);
+console.log(`Resta: ${x - y}`);
+console.log(`Multiplicación: ${x * y}`);
+console.log(`División: ${x / y}`);

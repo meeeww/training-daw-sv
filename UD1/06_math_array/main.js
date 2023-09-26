a64788a440fd1473fa270cbfe0b9a48008e6dc1a
meeeww <thezasm03@gmail.com>
2023-09-26 19:47:39 +0200

    Ejercicio 6 terminado

--- UD1/06_math_array/main.js
+++ UD1/06_math_array/main.js
@@ -4,11 +4,14 @@
 // El elemento más pequeño
 // La media de los elementos
 
-
+let array2 = [1, 2, 3]
 
 function doCalculation(array) {
-
-
+    console.log(`Suma de todos los elementos: ${array.reduce((numero1, numero2) => numero1 + numero2, 0)}`)
+    console.log(`El elemento más grande: ${Math.max(...array)}`)
+    console.log(`El elemento más pequeño: ${Math.min(...array)}`)
+    console.log(`La media de todos los elementos: ${array.reduce((numero1, numero2) => numero1 + numero2, 0) / array.length}`)
+    console.log("---")
 }
 
 

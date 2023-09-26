59e3440cc08cbc032d34176149093c836cd4f2e6
meeeww <thezasm03@gmail.com>
2023-09-26 20:42:52 +0200

    Ejercicio 13 terminado

--- UD1/13_multiplications/main.js
+++ UD1/13_multiplications/main.js
@@ -1,5 +1,10 @@
 function printMultiplicationTables() {
-
+    for(let i = 1; i < 10; i++){
+        for(let l = 1; l < 10; l++){
+            console.log(`${i * l} `)
+        }
+        console.log("\n")
+    }
 }
 
 printMultiplicationTables()

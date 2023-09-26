b9f0ab19505da9c2f1a0952581bf95717eb0de31
meeeww <thezasm03@gmail.com>
2023-09-26 19:57:39 +0200

    Ejercicio 7 hecho

--- UD1/07_insert_if_not_exists/main.js
+++ UD1/07_insert_if_not_exists/main.js
@@ -1,6 +1,9 @@
 const assert = require('assert').strict
 
-function insertIfNotExists(array, item, position) {
+function insertIfNotExists(array, fruta, posicion) {
+    if (!array.includes(fruta))
+    posicion ? array.unshift(fruta) : array.push(fruta);
+
     return array
 }
 
@@ -10,9 +13,8 @@ let array = ['pera', 'manzana']
 let result = insertIfNotExists(array, 'pera', false)
 assert.deepStrictEqual(result, ['pera', 'manzana'])
 
-// result = insertIfNotExists(array, 'melón', false)
-// assert.deepStrictEqual(result, ['pera', 'manzana', 'melón'])
+result = insertIfNotExists(array, 'melón', false)
+assert.deepStrictEqual(result, ['pera', 'manzana', 'melón'])
 
-// result = insertIfNotExists(array, 'melocotón', true)
-// assert.deepStrictEqual(result, ['melocotón', 'pera', 'manzana', 'melón'])
-
+result = insertIfNotExists(array, 'melocotón', true)
+assert.deepStrictEqual(result, ['melocotón', 'pera', 'manzana', 'melón'])

6971da8e48acdad9231343dd2d600be043d875e4
Zas <102923191+meeeww@users.noreply.github.com>
2023-10-18 18:32:59 +0200

    9 hecho

--- UD3/09_ajax/main.js
+++ UD3/09_ajax/main.js
@@ -1,14 +1,16 @@
-window.onload = function() {
-    console.log('document loaded');
+window.onload = function () {
+  console.log("document loaded");
 
-    var xhttp = new XMLHttpRequest();   
+  var xhttp = new XMLHttpRequest();
 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText)
-        }
-      };
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+    }
+  };
 
-    xhttp.open("GET", "http://dummy.restapiexample.com/api/v1/employees", true);
-    xhttp.send();
-}
+  var respuesta = xhttp.open("GET", "https://dummy.restapiexample.com/api/v1/employees", true);
+  xhttp.send();
+
+  console.log(respuesta);
+};
